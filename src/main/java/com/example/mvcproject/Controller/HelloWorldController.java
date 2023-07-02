8893267ef46aaf05605c66a96d9c08533ce24e9f
satya <satya143@gmail.com>
2023-07-02 10:08:29 +0530

    Added:make rest call to show Hello Mark fromBridgelabz

---
+++ src/main/java/com/example/mvcproject/Controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.example.mvcproject.Controller;
+
+import com.example.mvcproject.model.User;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/hello")
+
+public class HelloWorldController {
+
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+
+    }
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name")String name){
+
+        return " Hello " +  name  +   "From BridgeLabz";
+    }
+
+}
